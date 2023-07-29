3a6771a7cd5f4db1a0374eaade54f144bf0652eb
Ross Johnstone <rhjohnstone@gmail.com>
2023-07-29 11:58:23 +0900

    huge optimisations

--- polo/main.lua
+++ polo/main.lua
@@ -4,19 +4,32 @@ function love.load()
     time_taken = 0
     finished = false
     countdown = 3
-    current_polo = {"", "", "", ""}
-    all_keys = {}
+    num_mistakes = 0
+    correct = {"p", "o", "l", "o"}
+    current_index = 1
 end
 
 function love.keypressed(key)
     if key == "r" then
         love.load()
     elseif (countdown == 0) and (not finished) then
-        table.insert(all_keys, key)
-        for i = 1, 3, 1 do
-            current_polo[i] =  current_polo[i + 1]
+        local is_correct = key == correct[current_index]
+        if not is_correct then
+            num_mistakes = num_mistakes + 1
+            if key == "p" then
+                current_index = 2
+            else
+                current_index = 1
+            end
+        elseif current_index == 4 then
+            current_index = 1
+            radius = radius * 0.8
+            if radius <= 0.7 then
+                finished = true
+            end
+        else
+            current_index = current_index + 1
         end
-        current_polo[4] = key
     end
 end
 
@@ -41,8 +54,6 @@ function love.draw()
         love.graphics.setColor(1, 1, 1)
         local tt = tonumber(string.format("%.3f", time_taken))
         love.graphics.print("Finished in "..tt.." s", 50, 50)
-        local full_string = table.concat(all_keys)
-        local num_mistakes = count_mistakes(full_string)
         local s
         if num_mistakes == 1  then
             s = ""
@@ -66,45 +77,5 @@ function love.update(dt)
         end
     elseif not finished then
         time_taken = time_taken + dt
-        if table.concat(current_polo) == "polo" then
-            radius = radius * 0.8
-            if radius <= 0.7 then
-                finished = true
-                -- TODO: count_mistakes()
-            else
-                current_polo = {"", "", "", ""}
-            end
-        end
-    end
-end
-
-
-function count_mistakes(s, current_count)
-    current_count = current_count or 0
-    if s:len() == 0 then
-        return current_count
-    else
-        for i = 1, #s do
-            local c = s:sub(i, i)
-            if c ~= "p" then
-                current_count = current_count + 1
-            elseif s:sub(i+1, i+1) ~= "o" then
-                local remainder = s:sub(i+1)
-                current_count = current_count + 1
-                return count_mistakes(remainder, current_count)
-            elseif s:sub(i+2, i+2) ~= "l" then
-                local remainder = s:sub(i+2)
-                current_count = current_count + 1
-                return count_mistakes(remainder, current_count)
-            elseif s:sub(i+3, i+3) ~= "o" then
-                local remainder = s:sub(i+3)
-                current_count = current_count + 1
-                return count_mistakes(remainder, current_count)
-            else
-                local remainder = s:sub(i+4)
-                return count_mistakes(remainder, current_count)
-            end
-        end
-        return current_count
     end
 end
