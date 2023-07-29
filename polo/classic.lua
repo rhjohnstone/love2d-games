ac2b9c794caf8032e31edd0281d77ea43fe7a214
Ross Johnstone <rhjohnstone@gmail.com>
2023-07-29 23:23:43 +0900

    define game modes in separate files

---
+++ polo/classic.lua
@@ -0,0 +1,87 @@
+local classic = {}
+local correct = {"p", "o", "l", "o"}
+local end_radius = 0.7
+local radius_scale = 0.8
+
+function classic.load()
+    classic.current_index = 1
+    classic.num_mistakes = 0
+    classic.finished = false
+    classic.radius = 90
+    classic.countdown = 3
+    classic.countdown_time = 0
+    classic.time_taken = 0
+end
+
+function classic.keypressed(key)
+    if key == "r" then
+        classic.load()
+    else
+        local is_correct = key == correct[classic.current_index]
+        if not is_correct then
+            classic.num_mistakes = classic.num_mistakes + 1
+            if key == "p" then
+                classic.current_index = 2
+            else
+                classic.current_index = 1
+            end
+        elseif classic.current_index == 4 then
+            classic.current_index = 1
+            classic.radius = classic.radius * radius_scale
+            if classic.radius <= end_radius then
+                classic.finished = true
+            end
+        else
+            classic.current_index = classic.current_index + 1
+        end
+    end
+end
+
+function classic.update(dt)
+    if classic.countdown > 0 then
+        classic.countdown_time = classic.countdown_time + dt
+        if classic.countdown_time >= 1 then
+            classic.countdown = classic.countdown - 1
+            classic.countdown_time = 0
+        end
+    elseif not classic.finished then
+        classic.time_taken = classic.time_taken + dt
+    end
+end
+
+function classic.draw()
+    local font = love.graphics.newFont(36)
+    love.graphics.setFont(font)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print("Press R to retry", 50, 500)
+    if not classic.finished then
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.print("Type POLO as fast as you can", 50, 50)
+        local font = love.graphics.newFont(108)
+        love.graphics.setFont(font)
+        if classic.countdown > 0 then
+            love.graphics.setColor(1, 0, 0)
+            love.graphics.printf(classic.countdown, 400, 250, 300, "center")
+        else
+            love.graphics.setColor(0, 1, 0)
+            love.graphics.printf("GO!", 400, 250, 300, "center")
+        end
+    else
+        love.graphics.setColor(1, 1, 1)
+        local tt = string.format("%.3f", classic.time_taken)    
+        love.graphics.print("Finished in "..tt.." s", 50, 50)
+        local s
+        if classic.num_mistakes == 1  then
+            s = ""
+        else
+            s = "s"
+        end  
+        love.graphics.print("with "..classic.num_mistakes.." mistake"..s, 50, 100)
+    end
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.circle("fill", 200, 300, 100)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.circle("fill", 200, 300, classic.radius)
+end
+
+return classic
--- polo/main_old_working.lua
+++ polo/main_old_working.lua
@@ -3,6 +3,7 @@ local menu_options = {"Classic", "Survival", "Time trial"}
 local selected_menu_item = 1
 
 local classic = require("classic")
+print(classic)
 
 function love.load()
     window_width, window_height = love.graphics.getDimensions()
@@ -34,6 +35,17 @@ function draw_menu()
     end
 end
 
+function classic_load()
+    radius = 90
+    countdown_time = 0
+    time_taken = 0
+    finished = false
+    countdown = 3
+    num_mistakes = 0
+    correct = {"p", "o", "l", "o"}
+    current_index = 1
+end
+
 function time_trial_load()
     time_trial_time = 10
     start_radius = 90
@@ -59,9 +71,33 @@ function menu_keypressed(key)
     elseif key == 'return' or key == 'kpenter' then
         game_state = menu_options[selected_menu_item]
         if game_state == "Classic" then
-            classic.load()
+            classic_load()
         elseif game_state == "Time trial" then
             time_trial_load()
+        end
+    end
+end
+
+function classic_keypressed(key)
+    if key == "r" then
+        classic_load()
+    elseif (countdown == 0) and (not finished) then
+        local is_correct = key == correct[current_index]
+        if not is_correct then
+            num_mistakes = num_mistakes + 1
+            if key == "p" then
+                current_index = 2
+            else
+                current_index = 1
+            end
+        elseif current_index == 4 then
+            current_index = 1
+            radius = radius * 0.8
+            if radius <= 0.7 then
+                finished = true
+            end
+        else
+            current_index = current_index + 1
         end
     end
 end
@@ -72,7 +108,7 @@ function love.keypressed(key)
     elseif game_state  == "menu" then
         menu_keypressed(key)
     elseif game_state  == "Classic" then
-        classic.keypressed(key)
+        classic_keypressed(key)
     end
 end
 
@@ -80,11 +116,54 @@ function love.draw()
     if game_state == "menu" then
         draw_menu(menu_options, selected_menu_item, menu_font_height, window_width)
     elseif game_state == "Classic" then
-        classic.draw()
+        local font = love.graphics.newFont(36)
+        love.graphics.setFont(font)
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.print("Press R to retry", 50, 500)
+        if not finished then
+            love.graphics.setColor(1, 1, 1)
+            love.graphics.print("Type POLO as fast as you can", 50, 50)
+            local font = love.graphics.newFont(108)
+            love.graphics.setFont(font)
+            if countdown > 0 then
+                love.graphics.setColor(1, 0, 0)
+                love.graphics.printf(countdown, 400, 250, 300, "center")
+            else
+                love.graphics.setColor(0, 1, 0)
+                love.graphics.printf("GO!", 400, 250, 300, "center")
+            end
+        else
+            love.graphics.setColor(1, 1, 1)
+            local tt = string.format("%.3f", time_taken)    
+            love.graphics.print("Finished in "..tt.." s", 50, 50)
+            local s
+            if num_mistakes == 1  then
+                s = ""
+            else
+                s = "s"
+            end  
+            love.graphics.print("with "..num_mistakes.." mistake"..s, 50, 100)
+        end
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.circle("fill", 200, 300, 100)
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.circle("fill", 200, 300, radius)
     else
         love.graphics.setColor(1, 1, 1)
         love.graphics.print("Coming soon...", 50, 100)
         love.graphics.print("Press M for main menu.", 50, 500)
+    end
+end
+
+function classic_update(dt)
+    if countdown > 0 then
+        countdown_time = countdown_time + dt
+        if countdown_time >= 1 then
+            countdown = countdown - 1
+            countdown_time = 0
+        end
+    elseif not finished then
+        time_taken = time_taken + dt
     end
 end
 
@@ -106,7 +185,7 @@ end
 
 function love.update(dt)
     if game_state == "Classic" then
-        classic.update(dt)
+        classic_update(dt)
     elseif game_state == "Time trial" then
         time_trial_update(dt)
     end
