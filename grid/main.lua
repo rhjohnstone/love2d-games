6d8fb47bffa2f75e43ba6a12512bdccf847ed746
Ross Johnstone <rhjohnstone@gmail.com>
2023-08-02 15:33:18 +0900

    use actual objects, not name strings, in map

--- grid/main.lua
+++ grid/main.lua
@@ -15,6 +15,8 @@ function love.load()
     i, j = 8, 10
     x, y = cell2pixels(i, j)
 	block = {i=i, j=j, x=x, y=y}
+
+    -- From the tutorial: https://love2d.org/wiki/Tutorial:Gridlocked_Player
 	map = {
 		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
 		{ 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
@@ -30,14 +32,16 @@ function love.load()
 		{ 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
 		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
 	}
+
+    -- I convert it into my format
     for i, row in ipairs(map) do
         for j, v in ipairs(row) do
             if v == 1 then
                 row[j] = "wall"
             elseif (player.i == i) and (player.j == j) then
-                row[j] = "player"
+                row[j] = player
             elseif (block.i == i) and (block.j == j) then
-                row[j] = "block"
+                row[j] = block
             else
                 row[j] = false
             end
@@ -64,10 +68,10 @@ function love.draw()
                 love.graphics.setColor(1, 1, 1)
                 local x, y = cell2pixels(i, j)
                 love.graphics.rectangle("line", x, y, cell_size, cell_size)
-            elseif v == "player" then
+            elseif v == player then
                 love.graphics.setColor(0, 1, 0)
                 love.graphics.rectangle("fill", player.x, player.y, cell_size, cell_size)
-            elseif v == "block" then
+            elseif v == block then
                 love.graphics.setColor(0, 0, 1)
                 love.graphics.rectangle("fill", block.x, block.y, cell_size, cell_size)
             end
@@ -82,52 +86,52 @@ function love.keypressed(key)
         if not above then
             player.i = i - 1
             map[i][j] = false
-            map[i-1][j] = "player"
-        elseif (above == "block") and (not map[i-2][j]) then
+            map[i-1][j] = player
+        elseif (above == block) and (not map[i-2][j]) then
             player.i = i - 1
             map[i][j] = false
-            map[i-1][j] = "player"
+            map[i-1][j] = player
             block.i = block.i - 1
-            map[i-2][j] = "block"
+            map[i-2][j] = block
         end
     elseif key == "down" then
         local below = map[i + 1][j]
         if not below then
             player.i = i + 1
             map[i][j] = false
-            map[i+1][j] = "player"
-        elseif (below == "block") and (not map[i+2][j]) then
+            map[i+1][j] = player
+        elseif (below == block) and (not map[i+2][j]) then
             player.i = i + 1
             map[i][j] = false
-            map[i+1][j] = "player"
+            map[i+1][j] = player
             block.i = block.i + 1
-            map[i+2][j] = "block"
+            map[i+2][j] = block
         end
     elseif key == "left" then
         local left = map[i][j-1]
         if not left then
             player.j = j - 1
             map[i][j] = false
-            map[i][j-1] = "player"
-        elseif (left == "block") and (not map[i][j-2]) then
+            map[i][j-1] = player
+        elseif (left == block) and (not map[i][j-2]) then
             player.j = j - 1
             map[i][j] = false
-            map[i][j-1] = "player"
+            map[i][j-1] = player
             block.j = block.j - 1
-            map[i][j-2] = "block"
+            map[i][j-2] = block
         end
     elseif key == "right" then
         local right = map[i][j+1]
         if not right then
             player.j = j + 1
             map[i][j] = false
-            map[i][j+1] = "player"
-        elseif (right == "block") and (not map[i][j+2]) then
+            map[i][j+1] = player
+        elseif (right == block) and (not map[i][j+2]) then
             player.j = j + 1
             map[i][j] = false
-            map[i][j+1] = "player"
+            map[i][j+1] = player
             block.j = block.j + 1
-            map[i][j+2] = "block"
+            map[i][j+2] = block
         end
     end
 end
